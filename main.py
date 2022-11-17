e3ee9d5ac08145ca0ef7a4ce69e56d02f559aee7
resplit <67502036+aragha@users.noreply.github.com>
2022-11-17 15:56:33 +0000

    Completed the "odd / even" exercise on day 3

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+x = number % 2
+if x == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
