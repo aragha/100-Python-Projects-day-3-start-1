ac5a1930d6de2495be40d192a4d5a36e075705b8
resplit <67502036+aragha@users.noreply.github.com>
2022-11-17 16:25:34 +0000

    Completed the Exercise 2 - BMI 2.0 exercise

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-# print("Welcome to the rollercoaster!")
-# height = int(input("What is your height in cm? "))
+# # print("Welcome to the rollercoaster!")
+# # height = int(input("What is your height in cm? "))
+# # 🚨 Don't change the code below 👇
+# number = int(input("Which number do you want to check? "))
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# x = number % 2
+# if x == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
 # 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-x = number % 2
-if x == 0:
-    print("This is an even number.")
-else:
-    print("This is an odd number.")
+bmi = round(weight / (height * height))
+interpretations = ["are underweight.", 
+"have a normal weight.",
+"are slightly overweight",
+"are obese.",
+"are clinically obese."
+]
+# It should tell them the interpretation of their BMI based on the BMI value.
 
+# Under 18.5 they are underweight
+# Over 18.5 but below 25 they have a normal weight
+# Over 25 but below 30 they are slightly overweight
+# Over 30 but below 35 they are obese
+# Above 35 they are clinically obese.
+if bmi >= 35:
+    print(f"Your BMI is {bmi}, you {interpretations[4]}")
+else:
+  if bmi >= 30:
+    print(f"Your BMI is {bmi}, you {interpretations[3]}")
+  elif bmi >= 25:
+      print(f"Your BMI is {bmi}, you {interpretations[2]}")
+  elif bmi >= 18.5:
+      print(f"Your BMI is {bmi}, you {interpretations[1]}")    
+  else:
+      print(f"Your BMI is {bmi}, you {interpretations[0]}")        
